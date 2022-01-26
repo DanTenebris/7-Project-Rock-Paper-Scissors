da26dd2a74a12f81097d1e08bc82f2e5012ef0d0
Daniel <dan.tenebris@hotmail.com>
2022-01-26 12:17:03 -0300

    Se ha completado la interfaz del usuario cumpliendo con los requerimientos.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,123 +1,163 @@
-//La siguiente función, sin la necesidad de un parámetro, elegirá de forma aleatoria 'Rock', 'Paper' o 'Scissors'
+//DOM
+const body = document.querySelector('body');
+
+//Aquí van los botones
+const mainDiv = document.getElementsByClassName('main')[0];
+
+const button1 = document.getElementById('rock');
+const button2 = document.getElementById('paper');
+const button3 = document.getElementById('scissors');
+
+const reloadButton = document.createElement('button');
+
+reloadButton.setAttribute('id', 'rld');
+reloadButton.setAttribute('style', 'min-height: 70px');
+reloadButton.textContent = 'Play Again!';
+
+const articleOption = document.getElementsByClassName('optionDiv')[0];
+
+//Aquí van los mensajes
+const messageDiv = document.getElementsByClassName('messageDiv')[0];
+const messageP = document.getElementsByClassName('messageP')[0];
+
+const selectedDiv = document.createElement('article');
+mainDiv.appendChild(selectedDiv);
+selectedDiv.setAttribute('class', 'selectDiv');
+
+const playerSelection = document.createElement('p');
+const computerSelection = document.createElement('p');
+selectedDiv.appendChild(playerSelection);
+selectedDiv.appendChild(computerSelection);
+selectedDiv.style.display = 'none';
+
+const pWinnerAnnouncement = document.createElement('p');
+
+//Aquí va el score del juego
+const scoreTitleDiv = document.createElement('div');
+scoreTitleDiv.setAttribute('class', 'scoreTitle');
+mainDiv.appendChild(scoreTitleDiv);
+scoreTitleDiv.style.display = 'none';
+
+const scoreTitleP = document.createElement('p');
+scoreTitleDiv.appendChild(scoreTitleP);
+scoreTitleP.textContent = 'Score';
+
+const scoreDiv = document.createElement('div');
+scoreDiv.setAttribute('class', 'scoreDiv');
+mainDiv.appendChild(scoreDiv);
+scoreDiv.style.display = 'none';
+
+const playerScoreP = document.createElement('p');
+const computerScoreP = document.createElement('p');
+scoreDiv.appendChild(playerScoreP);
+scoreDiv.appendChild(computerScoreP);
+
+let playerGlobalScore = 0;
+let computerGlobalScore = 0;
+
+
+const buttons = document.querySelectorAll('button');
+
+//Identifica el id de los botones.
+buttons.forEach((button) => {
+
+  button.addEventListener('click', () => {
+    switch (button.id) {
+      case 'rock':
+        game('Rock');
+        break;
+      case 'paper':
+        game('Paper');
+        break;
+      case 'scissors':
+        game('Scissors');
+        break;
+      default:
+        break;
+    }
+  });
+
+});
+
+//Elige de forma aleatoria 'Rock', 'Paper' o 'Scissors'
 function computerPlay() {
-  //Crear un 'array' con las palabras 'Rock', 'Paper' y  'Scissors'
   let arregloRPS = ['Rock', 'Paper', 'Scissors'];
-  //Elegir un número aleatorio entre el uno y el tres
   let aleatorio = Math.floor(Math.random() * 3);
-  //Utilizar la palabra del 'array' dependiendo del número elegido y //Retornar el valor
   return arregloRPS[aleatorio];
 }
 
 //La siguiente función, recibiendo dos parámetros, elegirá cuál de ellos es el ganador de la ronda del juego
 function gameRound(playerSelection, computerSelection) {
-  //convertir todas las letras de los parámetros recibidos a minúsculas
+
   playerSelection = playerSelection.toLowerCase();
   computerSelection = computerSelection.toLowerCase();
 
-  //'If' ambos parámetros tienen el mismo valor, independientemente de su grafía (forma de representar los sonidos en las letras)
   if (playerSelection === computerSelection) {
-    //Escribir en una variable que el resultado es un empate
     return 'tie';
-    //'Else' (de lo contrario) revisar:
   } else {
 
-    //'If' playerSelection es igual a 'Rock':
     if (playerSelection === 'rock') {
-      //Entonces 
-      //'If' computerSelection es igual a 'Paper':
       if (computerSelection === 'paper') {
-        //Escribir como resultado que "Rock loses to Paper."
+        //"Rock loses to Paper."
         return false;
-        //'If' computerSelection es igual a 'Scissors':
       } else if (computerSelection === 'scissors') {
-        //Escribir "Rock beats Scissors."
+        //"Rock beats Scissors."
         return true;
       }
 
-      //'If' playerSelection es igual a 'Paper':
     } else if (playerSelection == 'paper') {
-
-      //Then
-      //'if' computerSelection es igual a 'Rock':
       if (computerSelection == 'rock') {
-        //Escribir "Paper beats Rock."
+        //"Paper beats Rock."
         return true;
-        //'if' computerSelection es igual a 'Scissors':
       } else if (computerSelection == 'scissors') {
-        //Escribir "Paper loses to Scissors."
+        //"Paper loses to Scissors."
         return false;
       }
 
-      //'If' playerSelection es igual a 'Scissors':
     } else if (playerSelection == 'scissors') {
-
-      //Then
-      //'if' computerSelection es igual a 'Rock':
       if (computerSelection == 'rock') {
-        //Escribir "Scissors loses to Rock."
+        //"Scissors loses to Rock."
         return false;
-        //'if' computerSelection es igual a 'Paper':
       } else if (computerSelection == 'paper') {
-        //Escribir "Scissors beats Paper."
+        //"Scissors beats Paper."
         return true;
       }
     }
   }
 }
 
+//Esta función vuelve visible los elementos ocultos.
+function displayElements() {
+  selectedDiv.style.display = 'grid';
+  scoreTitleDiv.style.display = 'grid';
+  scoreDiv.style.display = 'grid';
+  return;
+}
+
 //La siguiente función se encargará de contar las victorias o derrotas de las rondas.
-function game() {
-  //Crear la variable que guardará el score del jugador.
-  let playerScore = 0;
-  //Crear la variable que guardará el score del pc.
-  let computerScore = 0;
-  //Crear un ciclo de cinco vueltas en el que se muestre el contador de las veces que el jugador y la computadora an vencido a su oponente
-  for (let i = 0; i < 5; i++) {
-    //Crear una variable y solicitar al jugador que ingrese 'Rock', 'Paper' o 'Scissors'
-    let playerVal = prompt('Write down \'Rock\', \'Paper\' or \'Scissors\' to begin the game number ' + i + '.');
-    //Crear una variable en el que se guarde la elección al azar de la computadora.
-    let computerVal = computerPlay();
-    //Crear variable y guardar el resultado de la comparación en una función para comparar resultado del score.
-    let resultScore = gameRound(playerVal, computerVal);
-    //Publicar un mensaje adecuado luego de la comparación.
-    console.log('You: ' + playerVal + ', Computer: ' + computerVal);
-    console.log(messageResult(playerVal, computerVal, resultScore));
+function game(gameButtonValue) {
 
-    //Condicional if else para comparar y guardar el resultado.
-    if ((resultScore === true) && !(resultScore === 'tie')) {
-      ++playerScore;
-    }
-    if (resultScore === false) {
-      ++computerScore;
-    }
+  let playerVal = gameButtonValue;
+  let computerVal = computerPlay();
+  let resultScore = gameRound(playerVal, computerVal);
 
-    //Publicar el actual score.
-    console.log('Player: ' + playerScore + ', Computer: ' + computerScore);
-  }
+  displayElements();
 
-  //Luego de que acabe el ciclo verificar el puntaje del contador para saber quién de los dos jugadores ha ganado el juego.
-  if (playerScore < computerScore) {
-    return console.log('You lose de game.');
-  } else if (playerScore > computerScore) {
-    return console.log('You win.');
-  } else {
-    return console.log('Is it a tie?');
-  }
-  //Retornar un mensaje con el resultado correspondiente.
+  playerSelection.textContent = 'You selected ' + playerVal;
+  computerSelection.textContent = 'Computer selected ' + computerVal;
+  messageP.textContent = messageResult(playerVal, computerVal, resultScore);
+
+  compareGameScore(resultScore);
+  showWinner();
+
 }
 
 //La siguiente función verificará el resultado de la comparación de los resultados para luego retornar un mensaje adecuado.
 function messageResult(playerSelection, computerSelection, result) {
-  //verificando el resultado de otra función
-  // Si no es cero
   if (!(result === 'tie')) {
-    //'if' es verdadero:
     if (result) {
-      //escribir playerSelection + ' beats ' + computerSelection;
       return playerSelection + ' beats ' + computerSelection;
-      //'else'
     } else {
-      //escribir playerSelection + ' loses to ' + computerSelection;
       return playerSelection + ' loses to ' + computerSelection;
     }
   } else {
@@ -125,4 +165,44 @@ function messageResult(playerSelection, computerSelection, result) {
   }
 }
 
-game();
+
+function compareGameScore(roundResult) {
+
+  if ((roundResult) && !(roundResult === 'tie')) {
+    ++playerGlobalScore;
+
+  } else if (roundResult === false) {
+    ++computerGlobalScore
+  }
+  playerScoreP.textContent = 'Player Score: ' + playerGlobalScore;
+  computerScoreP.textContent = 'Computer Score: ' + computerGlobalScore;
+  return;
+}
+
+function showWinner() {
+
+  if (playerGlobalScore == 5) {
+    pWinnerAnnouncement.textContent = 'You Win.';
+    pWinnerAnnouncement.style.backgroundColor = 'green';
+    showFinalResult();
+
+  } else if (computerGlobalScore == 5) {
+    pWinnerAnnouncement.textContent = 'Game Over';
+    pWinnerAnnouncement.style.backgroundColor = '#8a0000';
+    showFinalResult();
+  } else {
+    return;
+  }
+
+}
+
+function showFinalResult() {
+  while (articleOption.firstChild) {
+    articleOption.removeChild(articleOption.lastChild);
+  }
+  mainDiv.appendChild(reloadButton);
+  articleOption.appendChild(pWinnerAnnouncement);
+  return reloadButton.addEventListener('click', () => {
+    location.reload();
+  });
+}
